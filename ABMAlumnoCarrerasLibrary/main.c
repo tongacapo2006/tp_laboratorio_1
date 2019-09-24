671e022ef85a3b8f31745db8d18dca59731156c0
tongacapo2006 <52983346+tongacapo2006@users.noreply.github.com>
2019-09-23 22:07:39 -0300

    Add files via upload

---
+++ ABMAlumnoCarrerasLibrary/main.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include "carrera.h"
+#include "alumno.h"
+
+#define TAM 10
+#define TAMC 3
+
+int menu();
+
+int main()
+{
+    int legajo = 20000;
+    eCarrera carreras[TAMC]={{1000,"TUP"},{1001,"TUSI"},{1002,"LIC"}};
+    eAlumno lista[TAM];
+    char salir = 'n';
+
+    inicializarAlumnos( lista, TAM);
+
+    legajo = legajo + hardcodearAlumnos(lista, TAM, 6);
+
+    do
+    {
+        switch( menu())
+        {
+        case 1:
+            if(altaAlumno(lista, TAM, legajo, carreras, TAMC))
+            {
+                legajo++;
+            }
+            break;
+
+        case 2:
+            bajaAlumno(lista, TAM, carreras, TAMC);
+            break;
+
+        case 3:
+            ModificarAlumno(lista, TAM, carreras, TAMC);
+            break;
+
+        case 4:
+            mostrarAlumnos(lista, TAM, carreras, TAMC);
+            break;
+
+        case 5:
+
+            ordenarAlumnos(lista, TAM);
+            break;
+
+        case 6:
+            printf("Informes Alumno\n\n");
+            break;
+        case 7:
+            mostrarCarreras(carreras, TAMC);
+            break;
+
+        case 8:
+            printf("Confirma salir?:");
+            fflush(stdin);
+            salir = getche();
+            break;
+
+        default:
+            printf("\nOpcion Invalida!\n\n");
+        }
+        system("pause");
+    }
+    while(salir == 'n');
+
+    return 0;
+}
+
+int menu()
+{
+    int opcion;
+
+    system("cls");
+    printf("****** ABM Alumnos *******\n\n");
+    printf("1-Alta alumno\n");
+    printf("2-Baja alumno\n");
+    printf("3-Modificar alumno\n");
+    printf("4-Listar alumnos\n");
+    printf("5-Ordenar alumnos\n");
+    printf("6-Informes alumno\n");
+    printf("7-Mostrar Carreras\n");
+    printf("8-Salir\n\n");
+    printf("Ingrese opcion: ");
+    scanf("%d", &opcion);
+
+    return opcion;
+}
+
+
+
